189dd75e75bfa8a7327d3a6efa87276c0b398f29
meep1w <kerentsev05@inbox.ru>
2025-10-03 10:17:08 +0300

    главное меню

--- bot/handlers/start.py
+++ bot/handlers/start.py
@@ -165,11 +165,10 @@ async def start_cmd(m: Message):
 
 async def send_main_menu(m: Message | CallbackQuery, nick: str | None = None):
     text = (
-        f"Привет{',' if nick else ''} <b>{html.escape(nick) if nick else m.from_user.full_name}</b>!\n\n"
-        f"Это магазин <a href=\"{html.escape(GROUP_URL or '#')}\">Slovekizna</a>.\n"
-        "Продвигайте свои соц.сети, каналы и воронки по лучшим ценам в любое время.\n"
-        f"Можете посетить мой <a href=\"{html.escape(PUBLIC_CHAT_URL or GROUP_URL or '#')}\">открытый чат</a> "
-        f"или ознакомиться с моей <a href=\"{html.escape(SCHOOL_URL or '#')}\">школой траффика</a>."
+        f"Привет{',' if nick else ''} <b>{html.escape(nick) if nick else m.from_user.full_name}</b>! это магазин SlovekinzShop\n\n"
+        f"Продвигай свои соц.сети, каналы и воронки по лучшим ценам с лучшим сервисом в любое время!\n"
+        f"Так же вы можете пообщаться в моем <a href=\"{html.escape(PUBLIC_CHAT_URL or GROUP_URL or '#')}\">открытом чате</a>,"
+        f"либо зайти в мой <a href=\"{html.escape(GROUP_URL or '#')}\">телеграм канал</a> на тематику Арбитража Трафика"
     )
     photo = FSInputFile(MENU_IMG) if MENU_IMG.exists() else None
     if isinstance(m, CallbackQuery):
